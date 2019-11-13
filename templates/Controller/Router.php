5aa6a4dbca54500b3d9a42cf70b4a83148084621
tvranjes <vranjes.tihomir@gmail.com>
2019-11-13 20:58:26 +0100

    Added router generator

---
+++ templates/Controller/Router.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace {VENDOR_NAME}\{MODULE_NAME}\Controller;
+
+use Magento\Framework\App\Action\Forward;
+use Magento\Framework\App\ActionFactory;
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\App\RouterInterface;
+
+/**
+ * Class Router
+ *
+ * @package {VENDOR_NAME}\{MODULE_NAME}\Controller
+ */
+class Router implements RouterInterface
+{
+    /**
+     * @var ActionFactory
+     */
+    private $actionFactory;
+
+    /**
+     * @var ResponseInterface
+     */
+    private $response;
+
+    /**
+     * Router constructor.
+     *
+     * @param ActionFactory $actionFactory
+     * @param ResponseInterface $response
+     */
+    public function __construct(
+        ActionFactory $actionFactory,
+        ResponseInterface $response
+    ) {
+        $this->actionFactory = $actionFactory;
+        $this->response = $response;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @return ActionInterface|null
+     */
+    public function match(RequestInterface $request): ?ActionInterface
+    {
+        return null;
+    }
+}
